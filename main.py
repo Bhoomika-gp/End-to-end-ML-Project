191f32adc4dd42c1808bf5b55309294c3ef04bf9
bhoomika.gp <bhoomikagp7@gmail.com>
2023-10-20 16:53:58 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger     #directly calling mlprjct instead of src aswell, cze i hv initializes logging functionality in __init__(constructor) 
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
