c8239a29ca5fa721382971c0560f86ad9b3c5216
bhoomika.gp <bhoomikagp7@gmail.com>
2023-10-20 12:04:37 +0530

    exception,  logger & utils added

---
+++ main.py
@@ -0,0 +1,3 @@
+from mlProject import logger     #directly calling mlprjct instead of src aswell, cze i hv initializes logging functionality in __init__(constructor) 
+
+logger.info("Welcome to our custom logging")
